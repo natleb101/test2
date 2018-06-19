471ccc3a0f9de7d23e08da6e4bb00e03801ec8e9
natleb101 <40369162+natleb101@users.noreply.github.com>
2018-06-19 14:24:49 -0400

    2nd change

--- simple analysis.R
+++ simple analysis.R
@@ -5,3 +5,13 @@ data("mtcars")
 str(mtcars)
 #first 5 observations
 head(mtcars)
+#last 5 observations
+tail(mtcars)
+# Number of rows (observations)
+nrow(mtcars)
+# Number of columns (variables)
+ncol(mtcars)
+#variable namesnames
+(mtcars)
+#descriptive stats for each variable
+summary(mtcars)
